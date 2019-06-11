6f51a5beec41e4d972d0ae9773e5904e4949c947
Jack Brennan <jack.brennan@nvngsystems.com>
2019-06-11 20:46:55 +0000

    Excercise 1 completed

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+#TODO - Use Active Record's create class method multiple times to create 3 stores in the database:
+
+store1 = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: TRUE, womens_apparel: TRUE)
+store2 = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: FALSE, womens_apparel: TRUE)
+store3 = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: TRUE, womens_apparel: FALSE)
+
+puts "Row count Store: #{Store.count}"
+
