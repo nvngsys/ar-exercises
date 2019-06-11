166e444ed5c622b526b8b94363ecead6fb1262aa
Jack Brennan <jack.brennan@nvngsystems.com>
2019-06-11 21:19:56 +0000

    Excercise 2 completed

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+#@store = find on stores where id = 1
+@store1 = Store.find(1);
+puts @store1
+puts @store1.name
+
+#@store = find on stores where id = 2
+@store2 = Store.find(2);
+puts @store2.name
+
+#update @store1 - change name
+@store1.name = 'Kamloops'
+@store1.save
