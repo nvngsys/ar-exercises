5ba17fc6bb48e51f275c9a1e739c30771f5d6689
Jack Brennan <jack.brennan@nvngsystems.com>
2019-06-12 01:13:13 +0000

    Excercise 7 completed

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,4 +1,12 @@
 class Employee < ActiveRecord::Base
   
     belongs_to :store
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :store, presence: true
+    validates :hourly_rate, length: {
+        minimum: 40,
+        maximum: 200
+    }
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,6 @@
 class Store < ActiveRecord::Base
 
     has_many :employees
+    validates :name, length: {minimum: 3}
+    validates :annual_revenue, length: {minimum: 0}, numericality: { only_integer: true }
 end
